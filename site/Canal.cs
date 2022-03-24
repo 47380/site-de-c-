8492e85171195951be3ce87b7597d6f6c09e5728
47380 <97446480+47380@users.noreply.github.com>
2022-03-24 18:14:04 -0300

    Update Canal.cs

--- site/Canal.cs
+++ site/Canal.cs
@@ -1,8 +1,13 @@
 using System;
 
-class Canal{
+public class Canal : IComparable<Canal>{
+  private string nome;
   public int Id { get; set; }
-  public string Nome { get ; set; }
+  public string Nome { get => nome; set => nome = value; }
+
+  public int CompareTo(Canal obj){
+    return this.nome.CompareTo(obj.nome);
+  }
 
   public override string ToString(){
     return $"{Id} - {Nome}";
